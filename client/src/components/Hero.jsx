5855d0f53b304b694de5fffa93fd62e6e4ad5e5d
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-05-27 21:39:14 +0530

    update the tailwind and finished navbar, setup hero section

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+
+function Hero() {
+  return (
+    <div className="container 2xl:px-20 mx-auto my-10">
+      <div className="bg-gradient-to-r from-green-800 to-purple-900 text-white py-16 text-center mx-2 rounded-xl">
+        <h2>Over 10,000+ jobs to apply</h2>
+        <p>
+          Your Next Big Move Starts Right Here - Explore The Best Job
+          Opportunities <br /> And Take The First Step Towards Your Dream Career
+        </p>
+        <div>
+          <div>
+            <img src={assets.search_icon} alt="" />
+            <input
+              type="text"
+              placeholder="Search for job"
+              className="max-sm:text-xs p-2 rounded outline-none w-full"
+            />
+          </div>
+          <div>
+            <img src={assets.location_icon} alt="" />
+            <input
+              type="text"
+              placeholder="Location"
+              className="max-sm:text-xs p-2 rounded outline-none w-full"
+            />
+          </div>
+          <button>Search</button>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,52 @@
-import React from 'react';
-import jobvoyage from '../assets/jobvoyage.jpg'
-import Button from '@mui/material/Button';
+import React from "react";
+import jobvoyage from "../assets/jobvoyage.jpg";
+import Button from "@mui/material/Button";
+import { useClerk, useUser, UserButton } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const { openSignIn } = useClerk();
+  const { user } = useUser();
+
   return (
-    <div className='shadow py-4'>
-      <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center'> 
-        <img src={jobvoyage} alt='logo' className='w-[100px] ml-[30px] rounded-4xl' />
-        <div className='flex gap-4 max-sm:text-xs'>
-            <Button variant="contained" color="primary" sx={{ borderRadius: '9999px', px: 3, mr: 2 }} size='small'>Recruiter Login</Button>
-            <Button variant='outlined' color="secondary" sx={{ borderRadius: '9999px', px: 3 }} size='small'>Login</Button>
-        </div>
+    <div className="shadow py-4">
+      <div className="container px-4 2xl:px-20 mx-auto flex ml-8 items-center justify-between">
+        <img
+          src={jobvoyage}
+          alt="logo"
+          className="w-[100px] ml-[30px] rounded-4xl"
+        />
+        {user ? (
+          <div className="flex items-center gap-3">
+            <Link to="/applications">Applied Jobs</Link>
+            <span>|</span>
+            <span>Hi, {user.firstName + " " + user.lastName}</span>
+            <UserButton />
+          </div>
+        ) : (
+          <div className="flex gap-4 max-sm:text-xs">
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ borderRadius: "9999px", px: 3, mr: 2 }}
+              size="small"
+            >
+              Recruiter Login
+            </Button>
+            <Button
+              onClick={() => openSignIn()}
+              variant="outlined"
+              color="secondary"
+              sx={{ borderRadius: "9999px", px: 3 }}
+              size="small"
+            >
+              Login
+            </Button>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
- 
