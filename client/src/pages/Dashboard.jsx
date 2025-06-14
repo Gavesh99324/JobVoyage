d6c290779545fffd4ce2cedbcf26a91a4f16e4bb
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-06-14 20:01:59 +0530

    update

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
-import jobvoyage from '../assets/jobvoyage.jpg'
-import {assets} from '../assets/assets'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import jobvoyage from "../assets/jobvoyage.jpg";
+import { assets } from "../assets/assets";
 
 const Dashboard = () => {
   return (
-    <div className='min-h-screen'>
-      <div className='shadow py-4'>
-        <div className='px-5 flex justify-between items-center'>
-          <img className='max-sm:w-32 cursor-pointer w-16 h-16 object-contain' src={jobvoyage} alt="" />
-          <div className='flex items-center gap-3'>
-            <p className='max-sm:hidden'>Welcome JobVoyage</p>
-            <div className='relative group'>
-              <img className='w-8 border rounded-full' src={assets.company_icon} alt="" />
-              <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
-                <ul> 
+    <div className="min-h-screen">
+      <div className="shadow py-4">
+        <div className="px-5 flex justify-between items-center">
+          <img
+            className="max-sm:w-32 cursor-pointer w-16 h-16 object-contain"
+            src={jobvoyage}
+            alt=""
+          />
+          <div className="flex items-center gap-3">
+            <p className="max-sm:hidden">Welcome JobVoyage</p>
+            <div className="relative group">
+              <img
+                className="w-8 border rounded-full"
+                src={assets.company_icon}
+                alt=""
+              />
+              <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12">
+                <ul>
                   <li>LogOut</li>
                 </ul>
               </div>
@@ -23,7 +31,7 @@ const Dashboard = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard;
