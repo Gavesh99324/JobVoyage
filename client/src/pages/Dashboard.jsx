93c4611ef3c6fafba2aec330ecc7c5ef10742e74
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-06-16 22:34:40 +0530

    update dashboard page sidebar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import jobvoyage from "../assets/jobvoyage.jpg";
 import { assets } from "../assets/assets";
 
 const Dashboard = () => {
+  const navigate = useNavigate(); // useNavigate() is a function that lets you navigate programmatically from one route to another.
+
   return (
     <div className="min-h-screen">
       <div className="shadow py-4">
         <div className="px-5 flex justify-between items-center">
           <img
+            onClick={() => navigate("/")}
             className="max-sm:w-32 cursor-pointer w-16 h-16 object-contain"
             src={jobvoyage}
             alt=""
@@ -22,12 +25,56 @@ const Dashboard = () => {
                 alt=""
               />
               <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12">
-                <ul>
-                  <li>LogOut</li>
+                <ul className="list-none m-0 p-2 bg-white rounded-md border text-sm">
+                  <li className="py-1 px-2 cursor-pointer pr-10">LogOut</li>
                 </ul>
               </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="flex items-start">
+        <div className="inline-block min-h-screen border-r-2">
+          <ul className="flex flex-col items-start pt-5 text-gray-800">
+            <NavLink
+              className={({ isActive }) =>
+                `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${
+                  isActive && "bg-blue-100 border-r-4 border-blue-500"
+                }`
+              }
+              to={"/dashboard/add-job"}
+            >
+              <img className="min-w-4" src={assets.add_icon} alt="" />
+              <p className="max-sm:hidden">Add Job </p>
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${
+                  isActive && "bg-blue-100 border-r-4 border-blue-500"
+                }`
+              }
+              to={"/dashboard/manage-job"}
+            >
+              <img className="min-w-4" src={assets.home_icon} alt="" />
+              <p className="max-sm:hidden">Manage Jobs</p>
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${
+                  isActive && "bg-blue-100 border-r-4 border-blue-500"
+                }`
+              }
+              to={"/dashboard/view-applications"}
+            >
+              <img className="min-w-4" src={assets.person_tick_icon} alt="" />
+              <p className="max-sm:hidden">View Applications</p>
+            </NavLink>
+          </ul>
+        </div>
+
+        <div>
+          <Outlet />
         </div>
       </div>
     </div>
